72b579895a7a11b496967f434c20b4f97a9ab2b6
RuyFr <82530881+RuyFr@users.noreply.github.com>
2021-09-21 21:49:48 -0500

    Create nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+        <header class="hero">
+        <div class="container">
+            <nav class="nav">
+                    <a href="index.php" class="nav__items nav__items--cta">INICIO</a>
+                    <a href="opinion.php" class="nav__items nav__items--cta">¿QUÉ OPINAS TU?</a>
+                    <a href="nosotros.php" class="nav__items nav__items--cta ">ACERCA DE NOSOTROS</a>                
+                </nav>
+            <section class="hero__container">
+                    <div class="hero__txt">
+                        <h1 class="hero__title">¡QUIENES SOMOS?</h1>
+                    </div>
+            </section>
+        </div>
+        <div class="hero__wave" style="overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width:100%"><path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00, 49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill:#fff;"></path></svg></div>
+   </header>
+
+   <main>
+
+    <section class="presentation container">
+        <h2 class="subtitle">EQUIPO NUMERO 3</h2>
+        <p class="presentation__copy">Nosotros somos el equipo numero 3 hablando del Derecho de la vida donde nosotros opinamos que es el derecho mas importante pues sin este no existirian los otros derechos<br><br>
+        Los integrantes son:<br><br>
+    <ol>
+    <li>Castillo Almazán Arturo Said</li><br>
+    <li>Fernández Martínez Brandon Shaiel</li><br>
+    <li>Fuentes Luna Jesús Axkana</li><br>
+    <li>Maldonado Soto Kitzaly Paola</li><br>
+    <li>Olvera Roman Fernando</li><br>
+      <li>Trujillo Romero Ethan Adair</li><br>
+</ol></p>
+   </section>
+
+    </body>
+    </html>
