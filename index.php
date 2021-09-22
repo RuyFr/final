acaa3f48e11d8e7256ea72adc7b593ce6fd4530c
RuyFr <82530881+RuyFr@users.noreply.github.com>
2021-09-21 21:49:03 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+   <header class="hero">
+        <div class="container">
+            <nav class="nav">
+                    <a href="index.php" class="nav__items nav__items--cta">INICIO</a>
+                    <a href="opinion.php" class="nav__items nav__items--cta">¿QUÉ OPINAS TU?</a>
+                    <a href="nosotros.php" class="nav__items nav__items--cta ">ACERCA DE NOSOTROS</a>                
+                </nav>
+            <section class="hero__container">
+                    <div class="hero__txt">
+                        <h1 class="hero__title">EL DERECHO DE LA VIDA</h1>
+                        <H2 class="hero__subtitle">¡Mi existencia digna, comienza en el vientre de mamá!</H2>
+                        <a href="#vida" class="hero__cta">SABER DEL DERECHO DE LA VIDA</a>
+                    </div>
+            </section>
+        </div>
+        <div class="hero__wave" style="overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width:100%"><path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00, 49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill:#fff;"></path></svg></div>
+   </header>
+
+   <main>
+
+    <section class="presentation container">
+        <img src="IMG/derecho.png" class="presentation__picture">
+        <h2 class="subtitle">LOS DERECHOS HUMANOS</h2>
+        <img src="IMG/d1.jpg" style="float: left; width: 50%; height: 50%;">
+        <p class="presentation__copy">Los Derechos Humanos son el conjunto de prerrogativas sustentadas en la dignidad humana, cuya realización efectiva resulta indispensable para el desarrollo integral de la persona. Este conjunto de prerrogativas se encuentra establecido dentro del orden jurídico nacional, en nuestra Constitución Política, tratados internacionales y las leyes.</p>
+       
+        <p class="presentation__copy">Los derechos humanos son derechos inherentes a todos los seres humanos, sin distinción alguna de nacionalidad, lugar de residencia, sexo, origen nacional o étnico, color, religión, lengua, o cualquier otra condición. Todos tenemos los mismos derechos humanos, sin discriminación alguna. Estos derechos son interrelacionados, interdependientes e indivisibles.</p>
+        <a href="https://www.cndh.org.mx/derechos-humanos/que-son-los-derechos-humanos" class="presentation__cta" target="_BLANK">Saber mas</a>
+   </section>
+
+       <section class="presentation container">
+            <img src="IMG/2.png" class="presentation__picture">
+            <h2 class="subtitle" id="vida">EL DERECHO DE LA VIDA</h2>
+            <img src="IMG/dos.jpg"  style="float: right; width: 50%; height: 50%;">
+            <p class="presentation__copy">El derecho a la vida se ha convertido en uno de los derechos fundamentales aceptado por muchos países; el 77 por ciento de las constituciones del mundo incluyen este derecho; en 1945, fecha en que se fundó la ONU, sólo el 27 por ciento de las constituciones vigentes lo contenían.</p>
+            <p class="presentation__copy">El derecho a la vida es un derecho universal, es decir que le corresponde a todo ser humano. Es un derecho necesario para poder concretizar todos los demás derechos universales. El derecho a la vida significa tener la oportunidad de vivir nuestra propia vida. Si no hay vida, no tiene sentido que existan los demás derechos fundamentales.</p>
+            <a href="https://www.humanium.org/es/derecho-vida/" target="_BLANK" class="presentation__cta">Saber mas</a>
+       </section>
+
+       <section class="people">
+
+        <div class="container">
+            <h2 class="subtitle">PERSONAS QUE LUCHARON</h2>
+            <div class="people__grid">
+
+                <article class="people__item">
+                     <img src="IMG/dori.jpeg" class="people__img">
+                     <div class="people__hover">
+                         <h2 class="people__title">Morena Herrera</h2>
+                         <a href="https://www.es.amnesty.org/en-que-estamos/blog/historia/articulo/morena-herrera-desafiando-la-prohibicion-total-del-aborto-en-el-salvador/" class="people__link" target="_BLANK">SABER MAS</a>
+                     </div>
+                </article>
+
+                <article class="people__item">
+                 <img src="IMG/gandi.jpeg" class="people__img">
+                 <div class="people__hover">
+                     <h2 class="people__title">Mahatma Gandhi</h2>
+                     <a href="https://www.unidosporlosderechoshumanos.mx/voices-for-human-rights/mahatma-gandhi.html" class="people__link" target="_BLANK">SABER MAS</a>
+                 </div>
+            </article>
+
+            <article class="people__item">
+             <img src="IMG/Eleanor.jpeg" class="people__img">
+             <div class="people__hover">
+                 <h2 class="people__title">Eleanor Roosvelt</h2>
+                 <a href="https://www.unidosporlosderechoshumanos.mx/voices-for-human-rights/eleanor-roosevelt.html" class="people__link" target="_BLANK">SABER MAS</a>
+             </div>
+        </article>
+
+        <article class="people__item">
+         <img src="IMG/jr.jpeg" class="people__img">
+         <div class="people__hover">
+             <h2 class="people__title">Martin Luther King Jr.</h2>
+             <a href="https://www.unidosporlosderechoshumanos.mx/voices-for-human-rights/martin-luther-king-jr.html" class="people__link" target="_BLANK">SABER MAS</a>
+         </div>
+    </article>
+
+    <article class="people__item">
+     <img src="IMG/mandela.jpeg" class="people__img">
+     <div class="people__hover">
+         <h2 class="people__title">Nelson Mandela</h2>
+         <a href="https://www.unidosporlosderechoshumanos.mx/voices-for-human-rights/nelson-mandela.html" class="people__link" target="_BLANK">SABER MAS</a>
+     </div>
+</article>
+
+            </div>
+        </div>
+    </section>
+
+       <section class="about container">
+            <div class="about__txt">
+                <h1 class="subtitle">Nuestas opiniones</h1>
+                <h2 class="subtitle">Jesus</h2>
+                <p class="about__paragraph">La ética en relación a la vida es fundamental, nos guía y nos instruye para ir por un camino mortalmente correcto. Así mismo, la vida es indispensable para la ética, para la existencia misma. Por ello, es tan importante que como seres humanos tengamos el derecho de vivir y tener una vida plena en la que no esté en tela de juicio ni con la incertidumbre de saber si en algún momento puede ser violado ese derecho.</p>
+                <p class="about__paragraph">En la relación de el derecho a vivir y la ética se pueden formar muchos argumentos y puntos de vista, sin embargo lo más importante es que se respete ése derecho, ya que todos lo merecemos desde el momento en que nos estamos engendrando, hasta el momento de nuestra muerte.</p>
+            </div>
+            <figure class="about__img">
+                    <img src="IMG/jesus.jpeg" class="about__picture">
+            </figure>
+            <div class="about__txt">
+                <h2 class="subtitle">Brandon</h2>
+                <p class="about__paragraph">Es un derecho qué cada persona tiene para traer a la vida a una persona pero tener la responsabilidad de darle una buena vida y digna, y a su vez vivir nuestra vida como nosotros queramos sin dañar a los demás</p>
+            </div>
+            <figure class="about__img about__img--left">
+                <img src="IMG/brandon.jpg" class="about__picture">
+        </figure>
+        <div class="about__txt">
+            <h2 class="subtitle">Paola</h2>
+            <p class="about__paragraph">Es justo que dejen que tengamos una vida desde saber que estamos dentro de el vientre, ya que yo no estoy a favor de los abortos y a mí conciederasion es una tontería que primero andén es sus cosas y luego no querían permitirle a un ser humano la vida que ellos mismos generaron.</p>
+        </div>
+        <figure class="about__img">
+            <img src="IMG/paola.jpg" class="about__picture">
+    </figure>
+        <div class="about__txt">
+            <h2 class="subtitle">Said</h2>
+            <p class="about__paragraph">El derecho a la vida es un derecho fundamental y universal, ya qué en lo personal creo que sin ese, no tendría sentido qué existieran los demás, es un derecho que significa tener la libertad y la oportunidad de vivir nuestra propia vida. 
+Este derecho lo ponemos en práctica desde que nacemos y este hace que nos desarrollemos, disfrutemos nuestra infancia, llegar a la edad y simplemente ser tu mismo, pero eso no significa exceder y violar los derechos de 3ras personas, simplemente vivir sin perjudicar a nadie.</p>
+        </div>
+        <figure class="about__img about__img--left">
+            <img src="IMG/said.jpeg" class="about__picture">
+    </figure>
+        <div class="about__txt">
+            <h2 class="subtitle">Ethan</h2>
+            <p class="about__paragraph">El derecho a la vida es algo que todo ser vivo tiene, y sin el se vería muy afectado el mundo en el que vivimos y por consecuente los de más derechos no tendrían mucho sentido, también es algo que desde chicos nos han enseñado, que cada ser tiene todo tipo de derechos y el de la vida es uno de los más importante</p>
+        </div>
+        <figure class="about__img">
+            <img src="IMG/ethan.jpeg" class="about__picture">
+    </figure>
+        <div class="about__txt">
+            <h2 class="subtitle">Fernando</h2>
+            <p class="about__paragraph"> El derecho a la vida es uno de los derechos universales el cual significa tener la oportunidad de vivir nuestra propia vida. Si no hay vida, no tiene sentido que existan los demás derechos fundamentales.</p>
+        </div>
+        <figure class="about__img about__img--left">
+            <img src="IMG/fernando.jpg" class="about__picture">
+    </figure>
+       </section>
+   </main>
+
+   <footer class="footer">
+       <div class="container footer__grid">
+           <h2 class="footer__txt"> SIGUENOS EN NUESTRAS REDES SOCIALES</h2>
+           <nav class="nav nav--footer">
+            <a href="" class="nav__items nav__items--footer"><ion-icon name="logo-facebook"></ion-icon>Facebook</a>
+            <a href="" class="nav__items nav__items--footer"><ion-icon name="logo-instagram"></ion-icon>Instagram</a>
+            <a href="" class="nav__items nav__items--footer"><ion-icon name="logo-twitter"></ion-icon>Twitter</a>
+           </nav>
+
+       </div>
+   </footer>
+
+
+   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
